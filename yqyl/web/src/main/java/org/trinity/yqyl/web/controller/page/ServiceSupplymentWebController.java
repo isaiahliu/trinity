7ccb7c7e22e6cd0a6d1b5ba1f4a20e998e7acbc6
Isaiah Liu <zqliu@hengtiansoft.com>
2016-09-18 17:58:43 +0800

    1

--- yqyl/web/src/main/java/org/trinity/yqyl/web/controller/page/ServiceSupplymentWebController.java
+++ yqyl/web/src/main/java/org/trinity/yqyl/web/controller/page/ServiceSupplymentWebController.java
@@ -1,6 +1,7 @@
 package org.trinity.yqyl.web.controller.page;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.trinity.common.exception.IException;
@@ -8,9 +9,14 @@ import org.trinity.common.exception.IException;
 @RestController
 @RequestMapping("/service/supplyment")
 public class ServiceSupplymentWebController extends AbstractResourceWebController {
-	@RequestMapping("")
-	public ModelAndView defaultPage() throws IException {
-		return createModelAndView("service/supplyment/default");
-	}
+    @RequestMapping(value = "/orders", method = RequestMethod.GET)
+    public ModelAndView ordersPage() throws IException {
+        return createModelAndView("service/supplyment/orders");
+    }
+
+    @RequestMapping(value = { "", "/serviceList" }, method = RequestMethod.GET)
+    public ModelAndView serviceListPage() throws IException {
+        return createModelAndView("service/supplyment/serviceList");
+    }
 
 }
