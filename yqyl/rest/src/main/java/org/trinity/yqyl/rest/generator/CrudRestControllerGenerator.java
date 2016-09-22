5d2c112d423fa9a87f0243c29af7132c5985dde7
isaia <isaia@DESKTOP-N4GCAFI>
2016-09-22 21:27:25 +0800

    1

--- yqyl/rest/src/main/java/org/trinity/yqyl/rest/generator/CrudRestControllerGenerator.java
+++ yqyl/rest/src/main/java/org/trinity/yqyl/rest/generator/CrudRestControllerGenerator.java
@@ -1,12 +1,7 @@
 package org.trinity.yqyl.rest.generator;
 
-import java.io.File;
-import java.io.IOException;
-
-import org.trinity.generator.CrudRestGenerator;
-
 public final class CrudRestControllerGenerator {
-	public static void main(final String[] args) throws IOException {
-		CrudRestGenerator.generate(new File("../"), "business");
-	}
+	// public static void main(final String[] args) throws IOException {
+	// CrudRestGenerator.generate(new File("../"), "business");
+	// }
 }
