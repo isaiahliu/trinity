e73bc070465aa4716646adb06e2a08e4b0c168f1
isaia <isaia@DESKTOP-N4GCAFI>
2016-09-26 20:07:33 +0800

    1

--- yqyl/web/src/main/java/org/trinity/yqyl/web/controller/page/HomeWebController.java
+++ yqyl/web/src/main/java/org/trinity/yqyl/web/controller/page/HomeWebController.java
@@ -8,14 +8,14 @@ import org.trinity.common.exception.IException;
 @RestController
 @RequestMapping
 public class HomeWebController extends AbstractResourceWebController {
-	@RequestMapping("")
-	public ModelAndView defaultPage() throws IException {
-		return createModelAndView("redirect:home");
-	}
+    @RequestMapping("")
+    public ModelAndView defaultPage() throws IException {
+        return createModelAndView("redirect:home");
+    }
 
-	@RequestMapping("/home")
-	public ModelAndView home() throws IException {
-		return createModelAndView("home");
-	}
+    @RequestMapping("/home")
+    public ModelAndView home() throws IException {
+        return createModelAndView("home");
+    }
 
 }
--- yqyl/web/src/main/resources/static/js/business/base/footer.js
+++ yqyl/web/src/main/resources/static/js/business/base/footer.js
@@ -1,11 +1,2 @@
 layoutApp.controller('footerController', function($scope, $http, $window) {
-	$scope.refresh = function() {
-		$http({
-			method : "PUT",
-			url : "/ajax/common/resource"
-		}).success(function(response) {
-			$window.location.reload();
-		}).error(function(response) {
-		});
-	};
 });
