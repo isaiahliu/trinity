992e3bf77daa9641e4b2f5a4c6e9e5cac89335b7
isaia <isaia@DESKTOP-N4GCAFI>
2016-09-26 22:01:45 +0800

    1

--- yqyl/web/src/main/resources/static/js/business/user/login.js
+++ yqyl/web/src/main/resources/static/js/business/user/login.js
@@ -1,26 +1,53 @@
 layoutApp.controller('loginController', function($scope, $http, $window) {
-	$scope.username = "";
-	$scope.password = "";
+	$scope.isReg = false;
+
+	$scope.loginData = {
+		username : "",
+		password : ""
+	};
 	$scope.login = function() {
 		$http({
 			method : "PUT",
 			url : "/ajax/user/authenticate",
-			data : {
-				username : $scope.username,
-				password : $scope.password
-			}
+			data : $scope.loginData
 		}).success(function(response) {
 			$window.location.href = "/home";
 		}).error(function(response) {
 
 		});
 	};
-	$scope.register = function() {
-		$window.location.href = "/user/register";
-	};
-	$scope.keydown = function(e) {
+
+	$scope.loginKeydown = function(e) {
 		if (e.keyCode == 13) {
 			$scope.login();
 		}
 	}
+
+	$scope.regKeydown = function(e) {
+		if (e.keyCode == 13) {
+			$scope.register();
+		}
+	}
+
+	$scope.registerData = {
+		username : "",
+		password : ""
+	};
+
+	$scope.repeatPassword = "";
+	
+	$scope.register = function() {
+		if ($scope.registerData.password != $scope.repeatPassword) {
+			return;
+		}
+
+		$http({
+			method : "POST",
+			url : "/ajax/user/register",
+			data : $scope.registerData
+		}).success(function(response) {
+			$window.location.href = "/user/login";
+		}).error(function(response) {
+		});
+	};
 });
--- yqyl/web/src/main/resources/static/js/business/user/register.js
+++ yqyl/web/src/main/resources/static/js/business/user/register.js
@@ -1,23 +1,3 @@
 layoutApp.controller('registerController', function($scope, $http, $window) {
-	$scope.username = "";
-	$scope.password = "";
-	$scope.repeatPassword = "";
-	$scope.register = function() {
-		if ($scope.password != $scope.repeatPassword) {
-			return;
-		}
-
-		$http({
-			method : "POST",
-			url : "/ajax/user/register",
-			data : {
-				username : $scope.username,
-				password : $scope.password
-			}
-		}).success(function(response) {
-			$window.location.href = "/user/login";
-		}).error(function(response) {
-
-		});
-	};
+	
 });
