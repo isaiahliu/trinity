55cef15f3abbd47d6ba277e9c3f7c45e17074766
Isaiah Liu <zqliu@hengtiansoft.com>
2016-09-28 18:16:20 +0800

    1

--- yqyl/web/src/main/resources/static/js/business/user/order.js
+++ yqyl/web/src/main/resources/static/js/business/user/order.js
@@ -1,3 +1,29 @@
 layoutApp.controller('orderController', function($scope, $http, $window) {
+	$scope.unprocessedOrderPopulated = false;
+	$scope.processingOrderPopulated = false;
+	$scope.processedOrderPopulated = false;
+
+	$scope.$watch('status', function(newValue, oldValue) {
+		switch (newValue) {
+		case "unprocessedOrder":
+			if (!$scope.unprocessedOrderPopulated) {
+				$scope.unprocessedOrderPopulated = true;
+			}
+			break;
+		case "processingOrder":
+			if (!$scope.processingOrderPopulated) {
+				$scope.processingOrderPopulated = true;
+			}
+			break;
+		case "processedOrder":
+			if (!$scope.processedOrderPopulated) {
+				$scope.processedOrderPopulated = true;
+			}
+			break;
+		default:
+			break;
+		}
+	});
+
 	$scope.status = 'unprocessedOrder';
 });
