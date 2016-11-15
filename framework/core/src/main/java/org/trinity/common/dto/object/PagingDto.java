30846874f4d284f48afb23ac5071190f018289ca
isaia <isaia@DESKTOP-N4GCAFI>
2016-11-15 22:19:59 +0800

    1

--- framework/core/src/main/java/org/trinity/common/dto/object/PagingDto.java
+++ framework/core/src/main/java/org/trinity/common/dto/object/PagingDto.java
@@ -6,63 +6,63 @@ package org.trinity.common.dto.object;
  * @author Isaiah Liu
  */
 public class PagingDto implements IPagingDto {
-    private static int DEFAULT_PAGE_INDEX = 0;
-    private static int DEFAULT_PAGE_SIZE = 10;
+	private static int DEFAULT_PAGE_INDEX = 0;
+	private static int DEFAULT_PAGE_SIZE = 99;
 
-    private int pageIndex = DEFAULT_PAGE_INDEX;
-    private int pageSize = DEFAULT_PAGE_SIZE;
-    private int pageCount;
-    private long itemCount;
+	private int pageIndex = DEFAULT_PAGE_INDEX;
+	private int pageSize = DEFAULT_PAGE_SIZE;
+	private int pageCount;
+	private long itemCount;
 
-    private String sortedBy;
+	private String sortedBy;
 
-    @Override
-    public long getItemCount() {
-        return itemCount;
-    }
+	@Override
+	public long getItemCount() {
+		return itemCount;
+	}
 
-    @Override
-    public int getPageCount() {
-        return pageCount;
-    }
+	@Override
+	public int getPageCount() {
+		return pageCount;
+	}
 
-    @Override
-    public int getPageIndex() {
-        return pageIndex;
-    }
+	@Override
+	public int getPageIndex() {
+		return pageIndex;
+	}
 
-    @Override
-    public int getPageSize() {
-        return pageSize;
-    }
+	@Override
+	public int getPageSize() {
+		return pageSize;
+	}
 
-    @Override
-    public String getSortedBy() {
-        return sortedBy;
-    }
+	@Override
+	public String getSortedBy() {
+		return sortedBy;
+	}
 
-    @Override
-    public void setItemCount(final long itemCount) {
-        this.itemCount = itemCount;
-    }
+	@Override
+	public void setItemCount(final long itemCount) {
+		this.itemCount = itemCount;
+	}
 
-    @Override
-    public void setPageCount(final int pageCount) {
-        this.pageCount = pageCount;
-    }
+	@Override
+	public void setPageCount(final int pageCount) {
+		this.pageCount = pageCount;
+	}
 
-    @Override
-    public void setPageIndex(final int pageIndex) {
-        this.pageIndex = pageIndex;
-    }
+	@Override
+	public void setPageIndex(final int pageIndex) {
+		this.pageIndex = pageIndex;
+	}
 
-    @Override
-    public void setPageSize(final int pageSize) {
-        this.pageSize = pageSize;
-    }
+	@Override
+	public void setPageSize(final int pageSize) {
+		this.pageSize = pageSize;
+	}
 
-    @Override
-    public void setSortedBy(final String sortedBy) {
-        this.sortedBy = sortedBy;
-    }
+	@Override
+	public void setSortedBy(final String sortedBy) {
+		this.sortedBy = sortedBy;
+	}
 }
