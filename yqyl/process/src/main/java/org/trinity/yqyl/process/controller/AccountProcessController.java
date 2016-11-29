4bf46ca0e76c02b6ace4165d439dd0a4a21924e6
isaia <isaia@DESKTOP-N4GCAFI>
2016-11-29 21:29:07 +0800

    1

--- yqyl/process/src/main/java/org/trinity/yqyl/process/controller/AccountProcessController.java
+++ yqyl/process/src/main/java/org/trinity/yqyl/process/controller/AccountProcessController.java
@@ -1,16 +1,42 @@
 package org.trinity.yqyl.process.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.trinity.common.exception.IException;
 import org.trinity.yqyl.common.message.dto.domain.AccountDto;
 import org.trinity.yqyl.common.message.dto.domain.AccountSearchingDto;
+import org.trinity.yqyl.common.message.lookup.AccountBalanceStatus;
+import org.trinity.yqyl.common.message.lookup.AccountCategory;
+import org.trinity.yqyl.common.message.lookup.AccountStatus;
 import org.trinity.yqyl.process.controller.base.AbstractAutowiredCrudProcessController;
 import org.trinity.yqyl.process.controller.base.IAccountProcessController;
+import org.trinity.yqyl.repository.business.dataaccess.IAccountBalanceRepository;
 import org.trinity.yqyl.repository.business.dataaccess.IAccountRepository;
 import org.trinity.yqyl.repository.business.entity.Account;
+import org.trinity.yqyl.repository.business.entity.AccountBalance;
 
 @Service
 public class AccountProcessController
-      extends AbstractAutowiredCrudProcessController<Account, AccountDto, AccountSearchingDto, IAccountRepository>
-      implements IAccountProcessController {
-}
+		extends AbstractAutowiredCrudProcessController<Account, AccountDto, AccountSearchingDto, IAccountRepository>
+		implements IAccountProcessController {
+	@Autowired
+	private IAccountBalanceRepository accountBalanceRepository;
 
+	@Override
+	public Account createAccount() throws IException {
+		final Account account = new Account();
+		account.setStatus(AccountStatus.ACTIVE);
+
+		getDomainEntityRepository().save(account);
+
+		final AccountBalance accountBalance = new AccountBalance();
+		accountBalance.setAmount(0d);
+		accountBalance.setCategory(AccountCategory.YIQUAN);
+		accountBalance.setStatus(AccountBalanceStatus.ACTIVE);
+		accountBalance.setAccount(account);
+
+		accountBalanceRepository.save(accountBalance);
+
+		return account;
+	}
+}
--- yqyl/process/src/main/java/org/trinity/yqyl/process/converter/ServiceSupplierClientConverter.java
+++ yqyl/process/src/main/java/org/trinity/yqyl/process/converter/ServiceSupplierClientConverter.java
@@ -24,99 +24,95 @@ import org.trinity.yqyl.repository.business.entity.ServiceSupplierClientMaterial
 @Component
 public class ServiceSupplierClientConverter extends AbstractLookupSupportObjectConverter<ServiceSupplierClient, ServiceSupplierClientDto> {
 
-    private static enum ServiceSupplierClientRelationship {
-        MATERIAL,
-        ACCOUNT,
-        SERVICE_INFOS,
-        AUDITINGS,
-        IMAGES,
-        NA
-    }
+	private static enum ServiceSupplierClientRelationship {
+		MATERIAL, BANK_ACCOUNT, SERVICE_INFOS, AUDITINGS, IMAGES, NA
+	}
 
-    @Autowired
-    private IObjectConverter<ServiceSupplierClientMaterial, ServiceSupplierClientMaterialDto> serviceSupplierClientMaterialConverter;
+	@Autowired
+	private IObjectConverter<ServiceSupplierClientMaterial, ServiceSupplierClientMaterialDto> serviceSupplierClientMaterialConverter;
 
-    @Autowired
-    private IObjectConverter<ServiceSupplierClientAccount, ServiceSupplierClientAccountDto> serviceSupplierClientAccountConverter;
+	@Autowired
+	private IObjectConverter<ServiceSupplierClientAccount, ServiceSupplierClientAccountDto> serviceSupplierClientAccountConverter;
 
-    @Autowired
-    private IObjectConverter<ServiceInfo, ServiceInfoDto> serviceInfoConverter;
+	@Autowired
+	private IObjectConverter<ServiceInfo, ServiceInfoDto> serviceInfoConverter;
 
-    @Autowired
-    private IObjectConverter<ServiceSupplierClientAuditing, ServiceSupplierClientAuditingDto> serviceSupplierClientAuditingConverter;
+	@Autowired
+	private IObjectConverter<ServiceSupplierClientAuditing, ServiceSupplierClientAuditingDto> serviceSupplierClientAuditingConverter;
 
-    @Autowired
-    public ServiceSupplierClientConverter(final IObjectConverter<Tuple2<ILookupMessage<?>, String[]>, LookupDto> lookupConverter) {
-        super(lookupConverter);
-    }
+	@Autowired
+	public ServiceSupplierClientConverter(final IObjectConverter<Tuple2<ILookupMessage<?>, String[]>, LookupDto> lookupConverter) {
+		super(lookupConverter);
+	}
 
-    @Override
-    protected void convertBackInternal(final ServiceSupplierClientDto source, final ServiceSupplierClient target,
-            final CopyPolicy copyPolicy) {
-        copyObject(source::getId, target::getUserId, target::setUserId, copyPolicy);
-        copyLookup(source::getStatus, target::getStatus, target::setStatus, ServiceSupplierClientStatus.class, copyPolicy);
-        copyLookup(source::getType, target::getType, target::setType, CompanyType.class, copyPolicy);
-        copyObject(source::getEmail, target::getEmail, target::setEmail, copyPolicy);
-        copyObject(source::getLogo, target::getLogo, target::setLogo, copyPolicy);
-        copyObject(source::getAddress, target::getAddress, target::setAddress, copyPolicy);
-        copyObject(source::getName, target::getName, target::setName, copyPolicy);
-        copyObject(source::getDescription, target::getDescription, target::setDescription, copyPolicy);
-        copyObject(source::getCategories, target::getServiceCategories, target::setServiceCategories, copyPolicy);
-        copyObject(source::getContact, target::getContact, target::setContact, copyPolicy);
-        copyObject(source::getContactPhone, target::getContactPhone, target::setContactPhone, copyPolicy);
-        copyObject(source::getRegion, target::getRegion, target::setRegion, copyPolicy);
-        copyObject(source::getServicePhone, target::getServicePhone, target::setServicePhone, copyPolicy);
-    }
+	@Override
+	protected void convertBackInternal(final ServiceSupplierClientDto source, final ServiceSupplierClient target,
+			final CopyPolicy copyPolicy) {
+		copyObject(source::getId, target::getUserId, target::setUserId, copyPolicy);
+		copyLookup(source::getStatus, target::getStatus, target::setStatus, ServiceSupplierClientStatus.class, copyPolicy);
+		copyLookup(source::getType, target::getType, target::setType, CompanyType.class, copyPolicy);
+		copyObject(source::getEmail, target::getEmail, target::setEmail, copyPolicy);
+		copyObject(source::getLogo, target::getLogo, target::setLogo, copyPolicy);
+		copyObject(source::getAddress, target::getAddress, target::setAddress, copyPolicy);
+		copyObject(source::getName, target::getName, target::setName, copyPolicy);
+		copyObject(source::getDescription, target::getDescription, target::setDescription, copyPolicy);
+		copyObject(source::getCategories, target::getServiceCategories, target::setServiceCategories, copyPolicy);
+		copyObject(source::getContact, target::getContact, target::setContact, copyPolicy);
+		copyObject(source::getContactPhone, target::getContactPhone, target::setContactPhone, copyPolicy);
+		copyObject(source::getRegion, target::getRegion, target::setRegion, copyPolicy);
+		copyObject(source::getServicePhone, target::getServicePhone, target::setServicePhone, copyPolicy);
+	}
 
-    @Override
-    protected void convertInternal(final ServiceSupplierClient source, final ServiceSupplierClientDto target, final CopyPolicy copyPolicy) {
-        copyObject(source::getUserId, target::getId, target::setId, copyPolicy);
-        copyMessage(source::getStatus, target::getStatus, target::setStatus, copyPolicy);
-        copyMessage(source::getType, target::getType, target::setType, copyPolicy);
-        copyObject(source::getEmail, target::getEmail, target::setEmail, copyPolicy);
-        copyObject(source::getName, target::getName, target::setName, copyPolicy);
-        copyObject(source::getAddress, target::getAddress, target::setAddress, copyPolicy);
-        copyObject(source::getLogo, target::getLogo, target::setLogo, copyPolicy);
-        copyObject(source::getDescription, target::getDescription, target::setDescription, copyPolicy);
-        copyObject(source::getServiceCategories, target::getCategories, target::setCategories, copyPolicy);
-        copyObject(source::getContact, target::getContact, target::setContact, copyPolicy);
-        copyObject(source::getContactPhone, target::getContactPhone, target::setContactPhone, copyPolicy);
-        copyObject(source::getRegion, target::getRegion, target::setRegion, copyPolicy);
-        copyObject(source::getServicePhone, target::getServicePhone, target::setServicePhone, copyPolicy);
-    }
+	@Override
+	protected void convertInternal(final ServiceSupplierClient source, final ServiceSupplierClientDto target, final CopyPolicy copyPolicy) {
+		copyObject(source::getUserId, target::getId, target::setId, copyPolicy);
+		copyMessage(source::getStatus, target::getStatus, target::setStatus, copyPolicy);
+		copyMessage(source::getType, target::getType, target::setType, copyPolicy);
+		copyObject(source::getEmail, target::getEmail, target::setEmail, copyPolicy);
+		copyObject(source::getName, target::getName, target::setName, copyPolicy);
+		copyObject(source::getAddress, target::getAddress, target::setAddress, copyPolicy);
+		copyObject(source::getLogo, target::getLogo, target::setLogo, copyPolicy);
+		copyObject(source::getDescription, target::getDescription, target::setDescription, copyPolicy);
+		copyObject(source::getServiceCategories, target::getCategories, target::setCategories, copyPolicy);
+		copyObject(source::getContact, target::getContact, target::setContact, copyPolicy);
+		copyObject(source::getContactPhone, target::getContactPhone, target::setContactPhone, copyPolicy);
+		copyObject(source::getRegion, target::getRegion, target::setRegion, copyPolicy);
+		copyObject(source::getServicePhone, target::getServicePhone, target::setServicePhone, copyPolicy);
+	}
 
-    @Override
-    protected void convertRelationshipInternal(final ServiceSupplierClient source, final ServiceSupplierClientDto target,
-            final RelationshipExpression relationshipExpression) {
-        switch (relationshipExpression.getName(ServiceSupplierClientRelationship.class)) {
-        case ACCOUNT:
-            copyRelationship(source::getBankAccount, target::setBankAccount, serviceSupplierClientAccountConverter, relationshipExpression);
-            break;
-        case MATERIAL:
-            copyRelationship(source::getMaterial, target::setMaterial, serviceSupplierClientMaterialConverter, relationshipExpression);
-            break;
-        case SERVICE_INFOS:
-            copyRelationshipList(source::getServiceInfos, target::setServiceInfos, serviceInfoConverter, relationshipExpression);
-            break;
-        case AUDITINGS:
-            copyRelationshipList(source::getAuditings, target::setAuditings, serviceSupplierClientAuditingConverter,
-                    relationshipExpression);
-            break;
-        case IMAGES:
-            copyObjectList(source::getImages, target::setImages);
-            break;
-        default:
-            break;
-        }
-    }
+	@Override
+	protected void convertRelationshipInternal(final ServiceSupplierClient source, final ServiceSupplierClientDto target,
+			final RelationshipExpression relationshipExpression) {
+		switch (relationshipExpression.getName(ServiceSupplierClientRelationship.class)) {
+			case BANK_ACCOUNT:
+				copyRelationship(source::getBankAccount, target::setBankAccount, serviceSupplierClientAccountConverter,
+						relationshipExpression);
+				break;
+			case MATERIAL:
+				copyRelationship(source::getMaterial, target::setMaterial, serviceSupplierClientMaterialConverter, relationshipExpression);
+				break;
+			case SERVICE_INFOS:
+				copyRelationshipList(source::getServiceInfos, target::setServiceInfos, serviceInfoConverter, relationshipExpression);
+				break;
+			case AUDITINGS:
+				copyRelationshipList(source::getAuditings, target::setAuditings, serviceSupplierClientAuditingConverter,
+						relationshipExpression);
+				break;
+			case IMAGES:
+				copyObjectList(source::getImages, target::setImages);
+				break;
+			default:
+				break;
+		}
+	}
 
-    @Override
-    protected ServiceSupplierClient createFromInstance() {
-        return new ServiceSupplierClient();
-    }
+	@Override
+	protected ServiceSupplierClient createFromInstance() {
+		return new ServiceSupplierClient();
+	}
 
-    @Override
-    protected ServiceSupplierClientDto createToInstance() {
-        return new ServiceSupplierClientDto();
-    }
+	@Override
+	protected ServiceSupplierClientDto createToInstance() {
+		return new ServiceSupplierClientDto();
+	}
 }
