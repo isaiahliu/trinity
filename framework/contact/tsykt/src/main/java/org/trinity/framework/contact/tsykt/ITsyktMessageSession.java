b6e314f5f8c7c0e9d0aac336c22823b52d93b3bc
Isaiah Liu <zqliu@hengtiansoft.com>
2017-02-06 14:19:49 +0800

    1

--- framework/contact/tsykt/src/main/java/org/trinity/framework/contact/tsykt/ITsyktMessageSession.java
+++ framework/contact/tsykt/src/main/java/org/trinity/framework/contact/tsykt/ITsyktMessageSession.java
@@ -3,5 +3,7 @@ package org.trinity.framework.contact.tsykt;
 import org.trinity.framework.contact.IContactMessageSession;
 
 public interface ITsyktMessageSession extends IContactMessageSession {
+    long getTimestamp();
 
+    void setTimestamp(long timestamp);
 }
