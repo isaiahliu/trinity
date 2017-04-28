dc910d6c7b3710486f7bf442de6cbc0f19c0c445
Isaiah <ziqiang.liu@enoch-car.com>
2017-04-28 15:33:05 +0800

    1

--- framework/core/src/main/java/org/trinity/common/dto/object/LookupResponse.java
+++ framework/core/src/main/java/org/trinity/common/dto/object/LookupResponse.java
@@ -1,9 +1,6 @@
 package org.trinity.common.dto.object;
 
-import org.trinity.common.dto.object.LookupDto;
 import org.trinity.common.dto.response.AbstractResponse;
 
 public class LookupResponse extends AbstractResponse<LookupDto> {
-
 }
-
